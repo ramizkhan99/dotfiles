230b75f6603f3385a33524c4c27cd6d1c5276f03
ramizkhan99 <ramizkhan1199@gmail.com>
2021-12-30 00:36:21 +0530

    Modified rofi launcher

--- rofi/launchers/colorful/launcher.sh
+++ rofi/launchers/colorful/launcher.sh
@@ -11,14 +11,14 @@
 # style_1     style_2     style_3     style_4     style_5     style_6
 # style_7     style_8     style_9     style_10    style_11    style_12
 
-theme="style_1"
+theme="style_8"
 dir="$HOME/.config/rofi/launchers/colorful"
 
 # dark
 ALPHA="#00000000"
-BG="#000000ff"
-FG="#FFFFFFff"
-SELECT="#101010ff"
+BG="#32302fbb"
+FG="#ebdbb2ff"
+SELECT="#282828ff"
 
 # light
 #ALPHA="#00000000"
@@ -29,7 +29,8 @@ SELECT="#101010ff"
 # accent colors
 COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
 		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
-ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
+#ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
+ACCENT="#98971a"
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
@@ -45,7 +46,7 @@ cat > $dir/colors.rasi <<- EOF
 EOF
 
 # comment these lines to disable random style
-themes=($(ls -p --hide="launcher.sh" --hide="colors.rasi" $dir))
-theme="${themes[$(( $RANDOM % 12 ))]}"
+#themes=($(ls -p --hide="launcher.sh" --hide="colors.rasi" $dir))
+#theme="${themes[$(( $RANDOM % 12 ))]}"
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
